c109fdaf4bafa2e877708e4bf0ac4a73472c8a31
Mykhailo Kondrashov <mykhailo.kondrashov@ihsmarkit.com>
2021-06-25 14:44:22 +0300

    new prop

--- Common/CommonClass.cs
+++ Common/CommonClass.cs
@@ -3,5 +3,6 @@
     public class CommonClass
     {
         public static string SomeProperty = "This is version 1 of some common property";
+        public static string SomeProperty2 = "This is version 2 of some common property";
     }
 }
